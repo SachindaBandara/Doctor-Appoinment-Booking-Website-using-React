979ff72b970a567f26dc65cae9fdee5b3af975b9
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-16 23:35:51 +0530

    add

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const SpecialityMenu = () => {
   return (
     <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='#speciality'>
-      <h1 className='text-3xl font-medium'>Find by Speciality</h1>
+      <h1 className='text-3xl font-bold'>Find by Speciality</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse thrugh our extensive list of trusted doctors. schedule your appointment...</p>
       <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
 
