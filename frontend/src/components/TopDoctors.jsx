df01dbb75c7f5b11fe6fd4f066626e3c6927ed13
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-16 23:14:20 +0530

    add top doctors

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { doctors } from '../assets/assets'
 
 const TopDoctors = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
+      <h1 className='text-3xl font-bold'>Top Doctors</h1>
+      <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
+      <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+        {doctors.slice(0,10).map((item,index)=>(
+            <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:tanslate-y-[-10px] transition-all duration-500' key={index}>
+                <img className='bg-blue-50 ' src={item.image} alt="" />
+                <div className='p-4'>
+                    <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p>
+                        <p>Available</p>
+                    </div>
+                    <p>{item.name}</p>
+                    <p>{item.speciality}</p>
+                </div>
+            </div>
+        ))}
+      </div>
+
+      <button>more</button>
     </div>
   )
 }
