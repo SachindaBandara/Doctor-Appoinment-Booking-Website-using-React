27105c5f7bfbe7f5a0a1222fa6fe2151bd651acf
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-16 22:00:53 +0530

    add

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <div>
-      
+      {/* Left side */}
+      <div>
+        <p>
+          Book Appointment <br /> With Trusted Doctors
+        </p>
+      </div>
+      {/* Right side */}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
