92f18317f7f456422239009d299718f44acba5d4
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-16 20:16:28 +0530

    add

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { assets } from "../assets/assets"
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
       <img className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
@@ -25,8 +28,8 @@ const NavBar = () => {
           <hr  className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
         </NavLink>
       </ul>
-      <div>
-        <button>Create Account</button>
+      <div className='flex items-center gap-4'>
+        <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
       </div>
     </div>
   )
