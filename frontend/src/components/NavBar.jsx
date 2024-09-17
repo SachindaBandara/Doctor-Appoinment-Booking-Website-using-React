735a667ed5f3b4f41e53c57b0ee28fd15fc570ea
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 23:12:33 +0530

    create mobile menu

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -49,6 +49,21 @@ const NavBar = () => {
           </div>
           : <button onClick={() => navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
         }
+
+        <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+        {/* Mobile menu */}
+        <div>
+          <div>
+            <img src={assets.logo} alt="" />
+            <img onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
+          </div>
+          <ul>
+            <NavLink>Home</NavLink>
+            <NavLink>Doctors</NavLink>
+            <NavLink>About</NavLink>
+            <NavLink>Contact</NavLink>
+          </ul>
+        </div>
        
       </div>
     </div>
