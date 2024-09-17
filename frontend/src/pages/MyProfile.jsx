048b533464bfa4d4f08644366cdc42f05b611ec4
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 22:24:22 +0530

    add

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -18,10 +18,11 @@ const MyProfile = () => {
   const [isEdit, setIsEdit] = useState(false);
 
   return (
-    <div>
-      <img src={userData.image} alt="" />
+    <div className="max-w-lg flex flex-col gap-2 text-sm">
+      <img className="w-36 rounded" src={userData.image} alt="" />
       {isEdit ? (
         <input
+        className="bg-gray-50 text-3xl font-medium max-w-60 mt-4"
           type="text"
           value={userData.name}
           onChange={(e) =>
@@ -29,13 +30,13 @@ const MyProfile = () => {
           }
         />
       ) : (
-        <p>{userData.name}</p>
+        <p className="font-medium text-3xl text-neutral-800 mt-4">{userData.name}</p>
       )}
 
-      <hr/>
+      <hr className="bg-zinc-400 h-[1px] border-none"/>
 <div>
-  <p>Contact Information</p>
-  <div>
+  <p className="text-neutral-500 underline mt-3">Contact Information</p>
+  <div className="grid grid-cols-[1fr_3fr] gray-y-2.5 mt-3 text-neutral-700">
     <p>Email Id :</p>
     <p>{userData.email}</p>
     <p>Phone</p>
