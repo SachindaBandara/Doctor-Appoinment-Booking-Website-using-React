70a9f5fb65dd331291daaf13c3bab238f66d59de
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 10:54:28 +0530

    add all changes

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,18 +1,18 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets";
 
-export const AppContext = createContext();
+export const AppContext = createContext()
 
-const AppContextProvider = () => {
+const AppContextProvider = (props) => {
 
     const value = {
         doctors
     }
 
     return (
-        <AppContextProvider value = {value}>
+        <AppContext.Provider value = {value}>
             {props.children}
-        </AppContextProvider>
+        </AppContext.Provider>
     )
 }
 
