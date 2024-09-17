f5436311446a30aa89d7dbd3f3218ce8963be5e4
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 11:40:52 +0530

    add all chnges

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,18 +3,18 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className=''>
-      <div>
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
         {/* Left section */}
         <div>
-            <img src={assets.logo} alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ducimus aliquid unde ipsam animi quidem debitis accusamus rem voluptatum cumque porro autem cum temporibus, veritatis, pariatur ea minima consequuntur aut!</p>
+            <img className='mb-5 w-40' src={assets.logo} alt="" />
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ducimus aliquid unde ipsam animi quidem debitis accusamus rem voluptatum cumque porro autem cum temporibus, veritatis, pariatur ea minima consequuntur aut!</p>
         </div>
 
         {/* Center section */}
          <div>
-            <p>COMPANY</p>
-            <ul>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-2 text-gray-600'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact Us</li>
@@ -24,19 +24,17 @@ const Footer = () => {
 
         {/* Right section */}
             <div>
-                <p>GET IN TOUCH</p>
-                <ul>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
                     <li>+94 77 587 9966</li>
                     <li>medhub@gmail.com</li>
                 </ul>
             </div>
-
-    
       </div>
       <div>
         {/* Copyright */}
         <hr/>
-        <p>Copyright @ 2024 MedHub - All right Reserved </p>
+        <p className='py-5 text-sm text-center'>Copyright @ 2024 MedHub - All right Reserved </p>
       </div>
     </div>
   )
