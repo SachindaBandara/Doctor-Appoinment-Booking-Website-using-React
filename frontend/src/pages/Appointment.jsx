2aed55d277ca1875e0af75c079940a0d3c562616
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 19:15:55 +0530

    add

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -142,13 +142,14 @@ const Appointment = () => {
                   </div>
                 ))}
             </div>
-            <div>
+            <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
               {docSlots.length && docSlots[slotIndex].map((item, index)=>(
-                <p key={index}>
-
+                <p onClick={()=>setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+                  {item.time.toLowerCase()}
                 </p>
               ))}
             </div>
+            <button className="bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6">Book an appointment</button>
           </div>
         </div>
       )
