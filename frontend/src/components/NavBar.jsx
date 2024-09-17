4b2387a86e9a73ad1be0cd811507504c0454b928
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 14:52:26 +0530

    add doctors filter

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-      <img className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
+      <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
 
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to='/'>
