5e89e764e61857c18baa48dffc0b0f18b531fc57
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 19:36:38 +0530

    create about us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      
+      <div>
+        <p>About <span>Us</span></p>
+      </div>
+
+      <div>
+        <img src={assets.about_image} alt="" />
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, magnam id. Provident voluptates atque, aliquid, eaque minus magnam optio ullam earum mollitia vitae, autem reprehenderit illum amet repellat est nemo!
+          </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet excepturi assumenda accusantium delectus possimus illum voluptatum labore recusandae, soluta adipisci natus et magnam aliquid, est quae fugiat mollitia ea reprehenderit?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit possimus illum minus omnis sapiente placeat voluptatibus, reiciendis eius animi quibusdam expedita esse aliquam, molestias, doloremque praesentium tenetur distinctio sit error.</p>
+        </div>
+      </div>
     </div>
   )
 }
