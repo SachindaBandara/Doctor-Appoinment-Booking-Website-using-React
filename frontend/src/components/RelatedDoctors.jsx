f0ed6e94e3f0599d33aa13154c33c62e674f92e9
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-17 19:33:40 +0530

    add

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,21 +1,62 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const RelatedDoctors = ({ speciality, docId }) => {
   const { doctors } = useContext(AppContext);
-  const [relDoc, setRelDoc] = useState([]);
+  const [relDocs, setRelDocs] = useState([]);
+  const navigate = useNavigate()
 
   useEffect(() => {
     if (doctors.length > 0 && speciality) {
       const doctorsData = doctors.filter(
         (doc) => doc.speciality === speciality && doc._id !== docId
       );
+      setRelDocs(doctorsData);
     }
   }, [doctors, speciality.docId]);
-  return;
-  <div>
 
-  </div>;
+  return (
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+      <h1 className="text-3xl font-bold">Top Doctors</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors.
+      </p>
+      <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+        {relDocs.slice(0, 5).map((item, index) => (
+          <div
+            onClick={() => {navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
+            className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+            key={index}
+          >
+            <img
+              className="bg-blue-50  hover:bg-primary"
+              src={item.image}
+              alt=""
+            />
+            <div className="p-4">
+              <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                <p className="w-2 h-2 bg-green-500 rounded-full"></p>
+                <p>Available</p>
+              </div>
+              <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+              <p className="text-gray-500 text-sm">{item.speciality}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 font-semibold px-12 py-3 rounded-full border border-blue-200 mt-10"
+      >
+        more
+      </button>
+    </div>
+  );
 };
 
 export default RelatedDoctors;
