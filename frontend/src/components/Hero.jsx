2bac1fbc74f6c6a40ac9654f3ec0e261838b0d03
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-18 14:08:30 +0530

    add

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,13 +7,13 @@ const Hero = () => {
       {/* Left side */}
       <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
         <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
-          Book Appointment <br /> With Trusted Doctors
+        Book Your Appointment <br /> With Leading Doctors
         </p>
-        <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
+        <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm">
             <img className="w-24" src={assets.group_profiles} alt=""/>
-            <p>Simply browser through our extensive list of <br className="hidden sm:block"/>trusted doctors...</p>
+            <p>Simply browse our comprehensive list of <br className="hidden sm:block"/>trusted doctors.</p>
         </div>
-        <a className="flex item-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300 " href="#speciality">
+        <a className="flex item-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm font-semibold m-auto md:m-0 hover:scale-105 transition-all duration-300 " href="#speciality">
             Book Appointment <img className="w-3" src={assets.arrow_icon} alt=""/>
         </a>
       </div>
