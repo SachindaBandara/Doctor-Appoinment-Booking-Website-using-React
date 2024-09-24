a3985fe9bfecf7ba7a27dbd1d7d2a351ca665fc1
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-24 21:04:02 +0530

    add

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -30,12 +30,12 @@ import doc12 from './doc12.png'
 import doc13 from './doc13.png'
 import doc14 from './doc14.png'
 import doc15 from './doc15.png'
-import Dermatologist from './Dermatologist.svg'
-import Gastroenterologist from './Gastroenterologist.svg'
-import General_physician from './General_physician.svg'
-import Gynecologist from './Gynecologist.svg'
-import Neurologist from './Neurologist.svg'
-import Pediatricians from './Pediatricians.svg'
+import Dermatologist from './Dermatologist.png'
+import Gastroenterologist from './Gastroenterologist.png'
+import General_physician from './General_physician.png'
+import Gynecologist from './Gynecologist.png'
+import Neurologist from './Neurologist.png'
+import Pediatricians from './Pediatricians.png'
 
 
 export const assets = {
